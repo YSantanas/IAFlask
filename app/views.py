fb9076c37d6721917b0e2ae7c2c2dcacd8aff182
YSantanas <44857937+YSantanas@users.noreply.github.com>
2021-09-09 00:15:21 -0500

    provisional LOGIN

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
 
+from .forms import LoginForm
+
 #blueprint permimte usar url
 pagina = Blueprint('pagina',__name__)
 
@@ -14,3 +16,9 @@ def pagina_no_encontrada(error):#parametro obligatorio, todo error regresa dos p
 @pagina.route('/')
 def index():
     return render_template('index.html', title='Inicio')
+
+    
+@pagina.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('auth/login.html', title='Login',form=form)
