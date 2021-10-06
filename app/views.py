9566b841cb2339e82fa9a1afa980e5104c00b1c2
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-10-06 00:10:35 -0500

    feat: return data tables to /read_csv2 endpoint

--- app/views.py
+++ app/views.py
@@ -10,8 +10,8 @@ from .forms import LoginForm
 
 # practica 3
 # _____________________________________
-# from scipy.spatial.distance import cdist    # Para el cálculo de distancias
-# from scipy.spatial import distance
+from scipy.spatial.distance import cdist    # Para el cálculo de distancias
+from scipy.spatial import distance
 # _____________________________________
 
 # blueprint permimte usar url
@@ -110,23 +110,111 @@ def read_csv():
 def read_csv2():
     if request.method == 'POST':
         flask_file = request.files['file']
+        input_a = int(request.form['input_a'])
+        input_b = int(request.form['input_b'])
 
         # if not a CSV file, return error
         if not flask_file.filename.endswith('.csv'):
             return make_response(jsonify({"message": "Seleccione un archivo CSV"}), 400)
 
-        archivo = pd.read_csv(flask_file, header=None)
-        print(archivo)
-        
+        archivo = pd.read_csv(flask_file)
+        # print(archivo)
+
         # Se crea una matriz (dataframe) usando la lista.
         nuevoArchivo = pd.DataFrame(archivo)
-        
+
         json_data = nuevoArchivo.to_json(orient='records')
-        
+
+        # _________________ENTRANDO EN PRACTICA3_________________
+        # _______________________________________________________
+        # -- se crea la matriz de distancia ECUCLIDIANA
+
+        DstEuclidiana = cdist(
+            archivo.iloc[0:10], archivo.iloc[0:10], metric='euclidean')
+        MEuclidiana = pd.DataFrame(DstEuclidiana)
+        # print(MEuclidiana)  # revisar que s emuestre en consola
+        # se desea que se muestre la distancia entre dos objetos
+        # esto da un valor numerico.
+
+        tabla_euclidiana = MEuclidiana.to_json(orient='records')
+
+        # se solicita el elemento, a comparar 1 y 2
+        Objeto1 = archivo.iloc[input_a]
+        Objeto2 = archivo.iloc[input_b]
+        dstEuclidiana = distance.euclidean(Objeto1, Objeto2)
+        # print(dstEuclidiana)  # este es el valor flotante retornado,
+
+        dist_euclidiana = float(dstEuclidiana)
+
+        # -- se crea la matriz de distancia para Chebyshev
+
+        DstChebyshev = cdist(
+            archivo.iloc[0:10], archivo.iloc[0:10], metric='chebyshev')
+        MChebyshev = pd.DataFrame(DstChebyshev)
+        # print(MChebyshev)
+
+        tabla_chebyshev = MChebyshev.to_json(orient='records')
+
+        # nuevamente se desea conocer la distancia entre dos objetos, seleccionados
+        # por el usuario.
+        # se solicita el elemento, a comparar 1 y 2
+        Objeto1 = archivo.iloc[input_a]
+        Objeto2 = archivo.iloc[input_b]
+        dstChebyshev = distance.chebyshev(Objeto1, Objeto2)
+        # print(dstChebyshev)  # valor numerico retornado
+
+        dist_chebyshev = float(dstChebyshev)
+
+        # -- se crea la matriz de distancia para Manhattan
+
+        DstManhattan = cdist(
+            archivo.iloc[0:10], archivo.iloc[0:10], metric='cityblock')
+        MManhattan = pd.DataFrame(DstManhattan)
+        # print(MManhattan)
+
+        tabla_manhattan = MManhattan.to_json(orient='records')
+
+        # nuevamente se desea conocer la distancia entre dos objetos, seleccionados
+        # por el usuario.
+
+        Objeto1 = archivo.iloc[input_a]
+        # se solicita el elemento, a comparar 1 y 2
+        Objeto2 = archivo.iloc[input_b]
+        dstManhattan = distance.cityblock(Objeto1, Objeto2)
+        # print(dstManhattan)
+
+        dist_manhattan = float(dstManhattan)
+
+        # -- se crea la matriz de distancia para Minkowski
+
+        DstMinkowski = cdist(
+            archivo.iloc[0:10], archivo.iloc[0:10], metric='minkowski', p=1.5)
+        MMinkowski = pd.DataFrame(DstMinkowski)
+        # print(MMinkowski)
+
+        tabla_minkowski = MMinkowski.to_json(orient='records')
+
+        # nuevamente se desea conocer la distancia entre dos objetos, seleccionados
+        # por el usuario.
+
+        Objeto1 = archivo.iloc[input_a]
+        # se solicita el elemento, a comparar 1 y 2
+        Objeto2 = archivo.iloc[input_b]
+        dstMinkowski = distance.minkowski(Objeto1, Objeto2, p=1.5)
+        # print(dstMinkowski)
+
+        dist_minkowski = float(dstMinkowski)
+
         return jsonify({
             "data": json_data,
-        #     "transactions": json_transactions,
+            "data_euclidiana": tabla_euclidiana,
+            "data_chebyshev": tabla_chebyshev,
+            "data_manhattan": tabla_manhattan,
+            "data_minkowski": tabla_minkowski,
+            "dist_euclidiana": dist_euclidiana,
+            "dist_chebyshev": dist_chebyshev,
+            "dist_manhattan": dist_manhattan,
+            "dist_minkowski": dist_minkowski
         })
-        
 
     return jsonify({'status': 'error', 'message': 'Error al leer el archivo'})
